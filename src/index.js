861c747f03c57429e0102cbba260e7b10bdcac68
black1277 <stumed@narod.ru>
2022-01-04 13:07:41 +0300

    add webhook

--- src/index.js
+++ src/index.js
@@ -3,20 +3,13 @@ const oneScene = require('./scenes/one')
 const twoScene = require('./scenes/two')
 require('dotenv').config()
 const BT = process.env.BOT_TOKEN
+const PORT = process.env.PORT || 5000
+const URL = process.env.URL
 const bot = new Telegraf(BT)
 
 const stage = new Scenes.Stage([oneScene, twoScene])
 bot.use(session({}))
 bot.use(stage.middleware())
-
-
-bot.hears('/one', (ctx) => ctx.reply('oneWizard'))
-
-bot.hears('Первый пункт', (ctx) => ctx.scene.enter('oneWizard'))
-bot.hears('Второй пункт', (ctx) => ctx.scene.enter('twoWizard'))
-
-bot.hears('Третий пункт', (ctx) => ctx.reply('Вы выбрали третий пункт!'))
-bot.hears('/stop', (ctx) => bot.stop('SIGINT'))
 
 bot.start(async (ctx) => {
   try {
@@ -29,7 +22,31 @@ bot.start(async (ctx) => {
   }
 })
 
-bot.launch()
+bot.hears('/one', (ctx) => ctx.reply('oneWizard'))
+
+bot.hears('Первый пункт', (ctx) => ctx.scene.enter('oneWizard'))
+bot.hears('Второй пункт', (ctx) => ctx.scene.enter('twoWizard'))
+
+bot.hears('Третий пункт', (ctx) => ctx.reply('Вы выбрали третий пункт!'))
+bot.hears('/stop', (ctx) => bot.stop('SIGINT'))
+
+bot.help((ctx) => ctx.reply(`Send me a sticker`))
+bot.on(`sticker`, (ctx) => ctx.reply(`👍`))
+bot.hears(`hi`, (ctx) => ctx.reply(`Hey there`))
+bot.command('env', (ctx) => {
+  ctx.reply(`ENV is ${process.env.NODE_ENV}`)
+})
+
+if (process.env.NODE_ENV === 'production') {
+  bot.telegram.setWebhook(`${URL}/bot${BT}`)
+  bot.startWebhook(`/bot${BT}`, null, PORT)
+  console.log('Started with webhook')
+} else {
+  bot
+    .launch()
+    .then((res) => console.log(`Launched at ${new Date()}`))
+    .catch((err) => console.log(`ERROR at launch:`, err))
+}
 
 process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
